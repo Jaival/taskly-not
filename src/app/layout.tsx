dcab4b692f89a3fcfb496359e03c55c09045efcc
Jaival Saija <saijajaival@gmail.com>
2024-04-12 00:11:46 +0200

    fix(components): added missing packages
    added missing packages and changed font for the website
    
    BREAKING CHANGE: added missing packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Rubik } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const rubik = Rubik({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Taskly',
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={rubik.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
 'use client';
-import { Button } from '@/components/ui/button';
-import { toast } from '@/lib/use-toast';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button
-        onClick={() => {
-          toast({
-            title: 'Scheduled: Catch up',
-            description: 'Friday, February 10, 2023 at 5:57 PM',
-          })
-        }}
-      >
-      Show Toast
-      </Button>
+      <div className="text-5xl">
+        Taskly
+      </div>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
       </div>
     </main>
